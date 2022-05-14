2d3073e65dd26eb8705e275db57f510e71b16b14
Rihard12 <rs1027@student.uni-lj.si>
2022-05-14 23:57:01 +0200

    Listing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import React from "react";
 import Listing from "./routes/listing";
 import AllListings from "./routes/allListings";
 import Login from "./routes/login";
--- frontend/src/routes/allListings.js
+++ frontend/src/routes/allListings.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function AllListings() {
     return (
-        <main style={{ padding: "1rem 0" }}>
-            <Link to="1" >
+        <main style={{ flexDirection: "row" }}>
+            <Link to="1">
                 <h2>Listing 1</h2>
             </Link>
-            <Link to="2" >
+            <Link to="2">
                 <h2>Listing 2</h2>
             </Link>
-            <Link to="3" >
+            <Link to="3">
                 <h2>Listing 3</h2>
             </Link>
             <Link to="4" >
                 <h2>Listing 4</h2>
             </Link>
-        </main>
+        </main >
     );
-}
+};
