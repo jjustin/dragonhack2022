400aea01aa09d59f993fa26d69a15894e05042bd
Janez Justin <janez.justin0@gmail.com>
2022-05-15 00:10:31 +0200

    more style 🔥🦄

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,22 +1,46 @@
-function H() {
+import { Link, useLocation } from 'react-router-dom'
+import { Header } from "antd/lib/layout/layout";
+import { useState, useEffect } from 'react';
 
-    <Header>
+let getLoginStatus = () => {
+    if (window.location.pathname.startsWith("/login") ||
+        window.location.pathname.startsWith("/register")) {
+        return 1
+    }
+    if (false) {
+        return 2
+    } // logged in
+    return 0
+}
+
+function H({ onChange }) {
+    let [loginStatus, setLoggedIn] = useState(getLoginStatus());
+    const location = useLocation();
+
+    useEffect(() => {
+        setLoggedIn(getLoginStatus());
+    }, [location]);
+
+    let login = null
+    if (loginStatus == 0) {
+        login = <Link to="/login" className="link">Login</Link>
+    }
+    if (loginStatus == 2) {
+        login = <Link to="/account" className="link">Ikonca do profila</Link>
+    }
+
+    return <Header>
         <div style={{ display: "flex" }}>
             <Link to="/" className="home-button link">
                 Triftify
             </Link>
             <div style={{ flexGrow: "1" }} />
 
-            {shouldHideLogin() ? null : <Link to="/login" className="link">Login</Link>}
+            {login}
         </div>
     </Header>
 }
 
-let shouldHideLogin = () => {
-    return window.location.pathname == "/login" ||
-        window.location.pathname == "/register" ||
-        false // hasToken
-}
 
 
-export default withRouter(H)
+export default H
