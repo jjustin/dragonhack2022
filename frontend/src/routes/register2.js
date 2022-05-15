27f7dbc9207aa662662a5e426b7f4e292ebe36c6
Janez Justin <janez.justin0@gmail.com>
2022-05-15 02:51:26 +0200

    login & register 😎

--- frontend/src/routes/register2.js
+++ frontend/src/routes/register2.js
@@ -7,70 +7,64 @@ export default function Login() {
     const [form] = Form.useForm();
 
     const onFinish = (values) => {
-        postApi('/login', values).
+        const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+
+
+        values.code = params.code
+        values.username = params.username
+
+        postApi('/verification', values).
             then(res => res.json()).
             then(res => {
                 if (res.error) {
                     form.setFields([
                         {
-                            name: 'password',
-                            errors: ['Username or password incorrect'],
+                            name: 'verification_code',
+                            errors: ['There is an error with validating your verification code'],
                         },
                     ])
                     return
                 }
+
                 localStorage.setItem("token", res.token);
                 localStorage.setItem("balance", res.balance);
                 window.location.href = "/";
             })
     };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Login Failed:', errorInfo);
-    };
-
     return (
         <div style={{ padding: "60px", textAlign: "center", width: "100%", flex: 1 }}>
             <Form
-                form={form} 
-                name="login"
+                form={form}
+                name="verification"
                 labelCol={{}}
                 wrapperCol={{}}
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
                 autoComplete="off"
                 style={{
                     maxWidth: "500px", margin: "auto"
                 }}
             >
+                <div>
+                    Enter the verification code you received via SMS
+                </div>
                 <Form.Item
-                    label="Username"
-                    name="username"
+                    label="Verification code"
+                    name="verification_code"
                     rules={[{ required: true, message: 'Please input your username!' }]}
                 >
                     <Input />
                 </Form.Item>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }, { min: 8, message: 'Password must be at least 8 characters' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{}}>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item> */}
-
                 <Form.Item wrapperCol={{}}>
                     <Button type="primary" htmlType="submit">
-                        Login
+                        Submit
                     </Button>
                 </Form.Item>
             </Form>
-            <div>Not yet a member? <Link to="/register">Register here</Link></div>
         </div >
     );
 }
