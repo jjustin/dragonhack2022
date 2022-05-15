a29dd12c04a7957a72cde8b5896751ff243c8535
Rihard12 <rs1027@student.uni-lj.si>
2022-05-15 08:59:04 +0200

    I do as Janez commands

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.post('/login', function (req, res) {
         res.send({ token: req.body.username, balance: users[req.body.username].coins_balance })
     } else {
         res.statusCode = 404
-        res.send({error: "404"})
+        res.send({ error: "404" })
     }
 });
 
@@ -49,7 +49,7 @@ app.post('/register', function (req, res) {
 
     if (users[req.body.username]) {
         res.statusCode = 409
-        res.send({error: "409"})
+        res.send({ error: "409" })
     } else {
         var verification_code = Math.floor(Math.random() * (10000 - 1000) + 1000)
         var public_code = Math.floor(Math.random() * (10000 - 1000) + 1000)
@@ -66,7 +66,7 @@ app.post('/register', function (req, res) {
             "verification_code": verification_code,
             "public_code": public_code,
         }
-        
+
         fs.writeFileSync('users.json', JSON.stringify(users));
         res.statusCode = 201
         res.send({ "status": "verification sent", "code": public_code, "username": req.body.username })
@@ -87,7 +87,7 @@ app.post('/verification', function (req, res) {
         res.send({ token: req.body.username, balance: users[req.body.username].coins_balance })
     } else {
         res.statusCode = 404
-        res.send({error: "404"})
+        res.send({ error: "404" })
     }
 });
 
@@ -99,11 +99,11 @@ app.post('/verification', function (req, res) {
 // poslje array z objekti zgoraj opisane oblike
 
 // user
-app.post('/user', function (req, res) { 
+app.get('/user', function (req, res) {
     const fs = require("fs")
     const users_file = fs.readFileSync("users.json")
     const users = JSON.parse(users_file)
-    res.send(users[req.body.username])
+    res.send(users[req.query.username])
 });
 
 // listing
@@ -112,13 +112,13 @@ app.get('/listings', function (req, res) {
     const users_file = fs.readFileSync("users.json")
     const users = JSON.parse(users_file)
     const allListings = [];
-    
+
     for (let i in users) {
         for (let j in users[i].listings) {
             allListings.push(users[i].listings[j])
         }
     }
-    
+
     res.send(allListings)
 });
 
@@ -134,7 +134,7 @@ app.post('/newlisting', function (req, res) {
     const fs = require("fs")
     const users_file = fs.readFileSync("users.json")
     const users = JSON.parse(users_file)
-    
+
     const id = randomUUID()
 
     let listing = {
@@ -160,7 +160,7 @@ app.post('/newlisting', function (req, res) {
 });
 
 app.get('/images', function (req, res) {
-    
+
     const name = req.query.name
     //console.log(name)
     const path = require('path');
@@ -194,58 +194,58 @@ const fileUpload = require('express-fileupload');
 // default options
 app.use(fileUpload());
 
-app.post('/upload', function(req, res) {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+app.post('/upload', function (req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  const imagename = req.query.imagename;
-  let sampleFile = req.files.imagefile;
-  const username = req.query.username;
-  const listingname = req.query.listingname;
-  const path = require('path');
-  let prelink = path.resolve('images'); // '/Users/joe/joe.txt' if run from my home folder
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    const imagename = req.query.imagename;
+    let sampleFile = req.files.imagefile;
+    const username = req.query.username;
+    const listingname = req.query.listingname;
+    const path = require('path');
+    let prelink = path.resolve('images'); // '/Users/joe/joe.txt' if run from my home folder
 
-  let link = path.join(prelink, imagename);
+    let link = path.join(prelink, imagename);
 
-  //console.log(link)
-  //console.log(sampleFile);
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(link, function(err) {
+    //console.log(link)
+    //console.log(sampleFile);
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(link, function (err) {
 
-    const fs = require("fs")
-    const users_file = fs.readFileSync("users.json")
-    const users = JSON.parse(users_file)
+        const fs = require("fs")
+        const users_file = fs.readFileSync("users.json")
+        const users = JSON.parse(users_file)
 
-    let temp = 0;
-    for (let i in users[username].listing) {
-        if (users[username].listing[i].imeListinga == listingname) {
-            
-            users[username].listing[i].imageName = imagename;
-            
-            fs.writeFileSync('users.json', JSON.stringify(users))
-            temp = 1;
-            break;
+        let temp = 0;
+        for (let i in users[username].listing) {
+            if (users[username].listing[i].imeListinga == listingname) {
+
+                users[username].listing[i].imageName = imagename;
+
+                fs.writeFileSync('users.json', JSON.stringify(users))
+                temp = 1;
+                break;
+            }
         }
-    }
-    if(temp == 0) {
-        return res.status(500).send("Ni listinga.");
+        if (temp == 0) {
+            return res.status(500).send("Ni listinga.");
 
-    }
+        }
 
-    if (err)
-      return res.status(500).send(err);
+        if (err)
+            return res.status(500).send(err);
 
-    res.send('File uploaded!');
-  });
+        res.send('File uploaded!');
+    });
 });
 
 
 
 
 
-    
+
 
 // // request
 // app.get('/request', function (req, res) { 
@@ -263,11 +263,11 @@ app.post('/seller', function (req, res) {  // body: seller username, buyer usern
     const comfirmations_file = fs.readFileSync("comfirmations.json")
     var comfirmations = JSON.parse(comfirmations_file)
     for (let i = 0; i < comfirmations.length; i++) {
-        if (comfirmations[i].seller == req.body.seller && comfirmations[i].buyer == req.body.buyer 
+        if (comfirmations[i].seller == req.body.seller && comfirmations[i].buyer == req.body.buyer
             && comfirmations[i].index == req.body.index) {
             comfirmations[i].seller_comf = true
             if (comfirmations[i].buyer_comf == true) {
-                
+
                 const users_file = fs.readFileSync("users.json")
                 var users = JSON.parse(users_file)
                 let price = users[req.body.seller].listing[req.body.index].cena
@@ -275,29 +275,29 @@ app.post('/seller', function (req, res) {  // body: seller username, buyer usern
                     users[req.body.buyer].coins_balance -= price
                     users[req.body.seller].coins_balance += price
                     fs.writeFileSync('users.json', JSON.stringify(users));
-                    comfirmations.splice(i,1)
+                    comfirmations.splice(i, 1)
                 } else {
                     res.statusCode = 404
-                    res.send({error: "404"})
+                    res.send({ error: "404" })
                 }
             }
             break
         }
     }
-    
+
     fs.writeFileSync('comfirmations.json', JSON.stringify(comfirmations));
-    res.send({"status": "ok"})
+    res.send({ "status": "ok" })
 
 });
 
 // buyer comfirmation
-app.post('/buyer', function (req, res) { 
+app.post('/buyer', function (req, res) {
     const fs = require("fs")
 
     const comfirmations_file = fs.readFileSync("comfirmations.json")
     var comfirmations = JSON.parse(comfirmations_file)
     for (let i = 0; i < comfirmations.length; i++) {
-        if (comfirmations[i].seller == req.body.seller && comfirmations[i].buyer == req.body.buyer 
+        if (comfirmations[i].seller == req.body.seller && comfirmations[i].buyer == req.body.buyer
             && comfirmations[i].index == req.body.index) {
             comfirmations[i].buyer_comf = true
             if (comfirmations[i].seller_comf == true) {
@@ -308,18 +308,18 @@ app.post('/buyer', function (req, res) {
                     users[req.body.buyer].coins_balance -= price
                     users[req.body.seller].coins_balance += price
                     fs.writeFileSync('users.json', JSON.stringify(users));
-                    comfirmations.splice(i,1)
+                    comfirmations.splice(i, 1)
                 } else {
                     res.statusCode = 404
-                    res.send({error: "404"})
+                    res.send({ error: "404" })
                 }
             }
             break
         }
     }
-    
+
     fs.writeFileSync('comfirmations.json', JSON.stringify(comfirmations));
-    res.send({"status": "ok"})
+    res.send({ "status": "ok" })
 });
 
 var server = app.listen(5000, function () {
